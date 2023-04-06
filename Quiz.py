00a5c7dd01e2a22410f8aa7e3b02e662816ac704
피스 <61900596+piese1028@users.noreply.github.com>
2023-04-06 09:58:25 +0900

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,13 @@
+print("문제: 고등학교 개교 연도는?")
+print("1. 1998년\t2. 1999년\n3. 2000년\t4. 2001년")
+
+answer = int(input("정답 입력 : "))
+
+if answer == 2:
+    print("정답입니다")
+    
+
+else:
+    print("오답입니다")
+
+print("퀴즈 종료")
